a217b8dd24cec3855257c587f57fa6d4e2d24a96
Leandro Cruz <leandro.programmer@hotmail.com>
2020-10-22 00:49:08 -0300

    localizacion de usuario terminado

--- App.tsx
+++ App.tsx
@@ -1,29 +1,57 @@
-import React, { useEffect } from "react";
-import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+// expo install react-native-maps
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+// expo install expo-location
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync,
 } from "expo-location";
 
+interface Position {
+  latitude: number;
+  longitude: number;
+}
+
 export default function App() {
+  const [position, setPosition] = useState<Position>();
+
   useEffect(() => {
     async function loadInitialPosition() {
       const { granted } = await requestPermissionsAsync();
 
       if (granted) {
-        const { coords } = await getCurrentPositionAsync({
-          // enableWighAccuracy: true
-        });
+        const { coords } = await getCurrentPositionAsync();
 
-        const { latitude, longitude } = coords;
+        setPosition(coords);
       }
     }
-    console.log("aqui");
+
     loadInitialPosition();
   }, []);
+  // Does not allow "position" to be possibly "undefined".
+  if (!position) {
+    return (
+      <View>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
 
-  return <MapView style={styles.map} />;
+  return (
+    <MapView
+      style={styles.map}
+      // Make IOS also use Google Maps
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation={true}
+      initialRegion={{
+        latitude: position.latitude,
+        longitude: position.longitude,
+        latitudeDelta: 0.008,
+        longitudeDelta: 0.008,
+      }}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ README.md
@@ -0,0 +1,21 @@
+# 🗺️ Function Current Location 🗺️
+
+## 🏁 Objetivo 🏁
+
+### Mi objetivo es que puedas usar esta función simples para implementarlo a tu proyecto que necesite buscar la localización actual del usuario que abrió la aplicación.
+
+## 👷 Para la cración de la Localización se utilizó 👷
+
+### 🔧 Expo
+
+### 🔧 React-Native
+
+### 🔧 TypeScript
+
+## 🕺 Clona este repositorio 🕺
+
+### Solo necesitas clonar el repositorio, extraerlo y abrirlo en tu editor de texto (por ejemplo, VSCode) y hacer un npm install (o yarn install) en tu terminal para instalar las dependencias. Luego para ejecutar el proyecto en tu terminal escribes: npm start (o yarn start) y todo listo. 👌
+
+## ⚠️ NO TE OLVIDES ⚠️
+
+- 😀 [Sigueme en Github 🙋‍♂️❤️](https://github.com/LeandroGCruzP)
